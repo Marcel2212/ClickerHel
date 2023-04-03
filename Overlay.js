d1741911d608123a514c4abedc05d57386592591
Marcel2212 <121469509+Marcel2212@users.noreply.github.com>
2023-04-03 14:56:31 +0200

    Update Overlay.js

--- Overlay.js
+++ Overlay.js
@@ -6,12 +6,12 @@
 
         let element2 = document.querySelector(".BattleTabStatisticComponentStyle-containerInsideTeams"); //Tab
         if (element2) {
-            element2.style.background = "rgb(0 0 0 / 75%)";
+            element2.style.background = "(100% 100% at 0% 0%, rgba(0, 170, 255, 0.75) 0%, rgba(255, 204, 0, 0) 100%), rgba(0, 20, 38, 0.75)";
         }
 
         let element3 = document.querySelector(".BattleTabStatisticComponentStyle-containerInsideResults"); //Tab 2 
         if (element3) {
-            element3.style.background = "rgb(0 0 0 / 75%)";
+            element3.style.background = "(100% 100% at 0% 0%, rgba(0, 170, 255, 0.75) 0%, rgba(255, 204, 0, 0) 100%), rgba(0, 20, 38, 0.75)";
         }
 
         let element4 = document.querySelector(".LobbyLoaderComponentStyle-container"); //Lobby Loading Screen
@@ -23,7 +23,7 @@
         if (element5) {
             element5.style.background = "radial-gradient(50% 100% at 50% 100%, rgb(20 131 241 / 67%) 0%, rgb(0 0 0 / 95%) 100%)";
          }
-        let element6 = document.querySelector(".ksc-361"); //Loading screen
+        let element6 = document.querySelector("x"); //Loading screen
         if (element6) {
            element6.style.background = "radial-gradient(100% 100% at 0% 0%, rgb(0 170 255 / 75%) 0%, rgba(255, 204, 0, 0) 100%), rgb(0 20 38 / 75%)";
         }
