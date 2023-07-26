0a493c621d52217b23ab09f4d907403cd6362f32
Marcel2212 <121469509+Marcel2212@users.noreply.github.com>
2023-07-26 21:12:48 +0200

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -5,7 +5,7 @@ function markTextsWithinSpan() {
   const turquoiseTags = ["[YSL]", "[CP9]", "[Mad]", "[Nike]", "naka", "Cube", "[1v9]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]"]; //allies
   
   const redTags = ["[KOA]", "[NL]", "[D3S]", "[sRev]", "[ly]", "[R8]", "[Do.it]", "[808]", "[K0A]", "Kuduro", "C.at", "Sarah.girl", "[HTP]", "[AR]", "[Nvm]", "[1994]", "[Cruel]", "[888]", "[Aura]", "[Q2]", "[99]", "[INRX]", "[CVS]", "[SXN]", "[6ang]", "[Way]", "[lKOAl]"
-                  , "[Bay]", "LLC", "Ms.Pwo", "[Ditsy]", "Batzen", "[44]", "[ZX]", "[VS]", "[Love]", "[2OP]", "[OL]", "[19]", "[RoRo]", "Okane", "Esco", "[20P]", "[[Volt]", "[C_F]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]"
+                  , "[Bay]", "LLC", "Ms.Pwo", "[Ditsy]", "Batzen", "[44]", "[ZX]", "[VS]", "[Love]", "[2OP]", "[OL]", "[19]", "[RoRo]", "Okane", "Esco", "[20P]", "[[Volt]", "[C_F]", "Lilli", "Glassy", " DESTROYER", "[xxxxxxx]", "[xxxxxxx]"
                   , "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]"
                   , "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]", "[xxxxxxx]"]; //enemys
 
